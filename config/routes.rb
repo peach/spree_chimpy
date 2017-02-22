fb03a2bf06b972d29d94476f244baac9b335abfe
Miguel Sancho <sanchocuba@gmail.com>
2017-02-21 21:51:59 -0800

    use routes.draw and not add_route in config/routes.rrb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Spree::Core::Engine.add_routes do
+# TODO: upgrade-solidus-2-1
+#Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.draw do
   namespace :chimpy, path: "" do
     resource :subscribers, only: [:create]
   end
